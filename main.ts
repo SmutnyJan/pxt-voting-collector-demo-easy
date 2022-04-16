b6721447771f32d699071e2c7cbd0e803387d4f8
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 15:24:38 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    server.showResults()
+})
+radio.onReceivedString(function (receivedString) {
+    server.addVote(receivedString, radio.receivedPacket(RadioPacketProperty.SerialNumber))
+})
+input.onButtonPressed(Button.B, function () {
+    server.newVoting()
+})
+radio.setGroup(1)
